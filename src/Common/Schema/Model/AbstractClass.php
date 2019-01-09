cfffa0b80e103a4a45cf2b274126319eb74c063e
Stavrakis Christos <c.stavrakis@intralot.com>
2019-01-09 13:44:23 +0200

    - Create ResourceModel class

--- src/Common/Schema/Model/AbstractClass.php
+++ src/Common/Schema/Model/AbstractClass.php
@@ -4,6 +4,9 @@ namespace ModelGenerator\Common\Schema\Model;
 
 use ModelGenerator\Common\Helper\Config;
 
+/**
+ *
+ */
 class AbstractClass
 {
     const MODEL_DIRECTORY = 'src/Model';
@@ -12,10 +15,12 @@ class AbstractClass
     protected $tableName;
     protected $tableColumns;
     protected $tablePrimaryKey;
+    protected $tableNameCamelize;
 
     public function __construct()
     {
         $this->clearModelDirectory();
+        $this->clearResourceModelDirectory();
     }
 
     public function initClass($tableName, $tableColumns)
@@ -24,19 +29,30 @@ class AbstractClass
         $this->tableName = $tableName;
         $this->tableColumns = $tableColumns['columns'];
         $this->tablePrimaryKey = $this->getPrimaryKey();
+        $this->tableNameCamelize = $this->camelize($this->tableName);
     }
 
     public function createClassFile()
     {
-        $this->writeFile();
+        $this->writeModelFile();
+        $this->writeResourceModelFile();
     }
 
-    protected function writeFile()
+    protected function writeModelFile()
     {
         $modelDir = Config::getModelDir();
-        $ourFileName = $modelDir .sprintf("/%s.php", $this->camelize($this->tableName));
+        $ourFileName = $modelDir .sprintf("/%s.php", $this->tableNameCamelize);
         $ourFileHandle = fopen($ourFileName, 'w') or die("can't open file");
         fwrite($ourFileHandle, $this->getClassTemplate());
+        fclose($ourFileHandle);
+    }
+
+    protected function writeResourceModelFile()
+    {
+        $modelDir = Config::getModelDir();
+        $ourFileName = $modelDir .sprintf("/ResourceModel/%s.php", $this->tableNameCamelize);
+        $ourFileHandle = fopen($ourFileName, 'w') or die("can't open file");
+        fwrite($ourFileHandle, $this->getResourceClassTemplate());
         fclose($ourFileHandle);
     }
 
@@ -50,6 +66,17 @@ class AbstractClass
         //TODO
         $modelDir = Config::getModelDir();
         $files = glob($modelDir.'/*');
+        foreach($files as $file){ // iterate files
+            if(is_file($file))
+                unlink($file); // delete file
+        }
+    }
+
+    protected function clearResourceModelDirectory()
+    {
+        //TODO
+        $modelDir = Config::getModelDir();
+        $files = glob($modelDir.'/ResourceModel/*');
         foreach($files as $file){ // iterate files
             if(is_file($file))
                 unlink($file); // delete file
@@ -75,10 +102,10 @@ class AbstractClass
         return $this->tableColumns;
     }
 
-    protected function getClassTemplate()
+    protected function getResourceClassTemplate()
     {
         return sprintf('<?php
-namespace ModelGenerator\Model;
+namespace ModelGenerator\Model\ResourceModel;
 
 use Zend\Db\Sql\Sql;
 use Zend\Db\TableGateway\AbstractTableGateway;
@@ -125,6 +152,26 @@ class %s extends AbstractTableGateway
             throw new \Exception($e->getPrevious()->getMessage());
         }
     }
-}',$this->camelize($this->tableName), $this->tableName );
+}', $this->tableNameCamelize, $this->tableName );
+    }
+
+    protected function getClassTemplate()
+    {
+
+        return sprintf('<?php
+namespace ModelGenerator\Model;
+
+
+class %s extends AbstractModel
+{
+
+
+    public function __construct()
+    {
+       $this->setResourceModel(%s);
+    }
+
+    
+}', $this->tableNameCamelize, $this->tableNameCamelize );
     }
 }
